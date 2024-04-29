ae37c9897c7a57399025763dc4ec6e512b380080
Gideon Mutai <giddymutai13@gmail.com>
2024-04-29 13:46:51 +0300

    Finished task

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,19 +1,21 @@
 // Code EyesOnMe Component Here
-import React from 'react'
+import React from "react";
 
 function EyesOnMe() {
-    function handleFocus() {
-        console.log('Good!')
-    }
+  function handleFocus() {
+    console.log("Good!");
+  }
 
-    function handleBlur() {
-        console.log('Hey! Eyes on me!')
-    }
+  function handleBlur() {
+    console.log("Hey! Eyes on me!");
+  }
   return (
     <div>
-        <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+      <button onFocus={handleFocus} onBlur={handleBlur}>
+        Eyes on me
+      </button>
     </div>
-  )
+  );
 }
 
-export default EyesOnMe
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,11 +1,14 @@
 // Code Keypad Component Here
 
-function Keypad (){
-    return (
-        <div>
-            <input type="password" onChange={() => console.log("Entering password...")}></input>
-        </div>
-    )
+function Keypad() {
+  return (
+    <div>
+      <input
+        type="password"
+        onChange={() => console.log("Entering password...")}
+      ></input>
+    </div>
+  );
 }
 
 export default Keypad;
